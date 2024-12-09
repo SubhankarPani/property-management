c792ee0258dca35c58729168ca29f1de8c1e8807
Subhankar Pani <subhankarpani10@gmail.com>
2024-12-09 19:20:41 +0530

    initial commit

---
+++ src/main/java/com/mycompany/property_management/entity/PropertyEntity.java
@@ -0,0 +1,78 @@
+package com.mycompany.property_management.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="PROPERTY_TABLE")
+@NoArgsConstructor
+public class PropertyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name="PROPERTY_TITLE",nullable = false)
+    private String title;
+    private String description;
+    private String ownerName;
+    @Column(name="EMAIL",nullable = false)
+    private String ownerEmail;
+    private Double price;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
